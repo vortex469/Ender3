30bdab41b1ff9f6d6e25d598561ea2e70890e1bf
vortex469 <kennyhorner@gmail.com>
2021-04-23 19:35:55 -0700

    Update Configuration.h
    Updated X and y steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 108.1 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.4, 80.7, 400, 108.1 }
 
 /**
  * Default Max Feed Rate (mm/s)
